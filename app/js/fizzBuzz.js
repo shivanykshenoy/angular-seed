124708e16402db39164f0747f5f5a14bc181ea0d
shivanyshenoy <shivanybhat@gmail.com>
2013-01-30 11:20:26 -0600

    initial commit

---
+++ app/js/fizzBuzz.js
@@ -0,0 +1,11 @@
+(function (angular) {
+    'use strict';
+
+    var ngModule = angular.module('fizzBuzz', []);
+    ngModule.factory('fizzBuzz', function fizzBuzzFactory() {
+        return function fizzBuzz() {
+
+        };
+    });
+
+}(window.angular));
